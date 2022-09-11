9fca7747ebe1176762074a1e29e64117738c2014
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-11 16:22:15 +0530

    import java.util.Scanner; class Main {
    	public static void main(String args[]) 
    	{
    		
    		Scanner sc=new Scanner(System.in);
    		int n,k=0;
    		n=sc.nextInt();
    		for(int i=1;i<=n;i++)			
    		{   
    			for(int j=1;j<=n;j++)		
    			{
    				System.out.print((char)(64+i)+" ");
    			}
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
-class Main
+class Main 
 {
+
 	public static void main(String args[]) 
 	{
+		
 		Scanner sc=new Scanner(System.in);
-		int r;
-		r=sc.nextInt();
-		for(int i=1;i<=2*r/2;i++)			 
-		{  
-		    int k=i>r?r-i%r:i;
-			for(int j=1;j<=r;j++)	 
+		int n,k=0;
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)			
+		{   
+			for(int j=1;j<=n;j++)		
 			{
-				if(j<=r-k)
-					System.out.print(" ");
+				System.out.print((char)(64+i)+" ");
 			}
-			for(int p=1;p<=2*k-1;p++)
-				System.out.print(i);
-			
 			System.out.println();
 		}
 	}
