5dbc5ace0960638336239b9223978fe709dd6319
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-11 16:16:02 +0530

    import java.util.Scanner; class Main { 	public static void main(String args[]) 	{ 		Scanner sc=new Scanner(System.in); 		int r; 		r=sc.nextInt(); 		for(int i=1;i<=2*r/2;i++) 		{ 		    int k=i>r?r-i%r:i; 			for(int j=1;j<=r;j++) 			{ 				if(j<=r-k) 					System.out.print(" "); 			} 			for(int p=1;p<=2*k-1;p++) 				System.out.print(i);
    			System.out.println();
    		}
    	}
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-class Main 
+class Main
 {
-  public static void main(String[] args)
+	public static void main(String args[]) 
 	{
-		
 		Scanner sc=new Scanner(System.in);
-		int n;
-		n=sc.nextInt();
-		for(int i=1;i<=n;i++)			
-		{
-			for(int j=n;j<=n && j>0;j--)
-			  
+		int r;
+		r=sc.nextInt();
+		for(int i=1;i<=2*r/2;i++)			 
+		{  
+		    int k=i>r?r-i%r:i;
+			for(int j=1;j<=r;j++)	 
 			{
-				System.out.print(j+" ");
+				if(j<=r-k)
+					System.out.print(" ");
 			}
+			for(int p=1;p<=2*k-1;p++)
+				System.out.print(i);
+			
 			System.out.println();
 		}
 	}
