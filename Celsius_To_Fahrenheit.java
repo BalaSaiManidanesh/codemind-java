2c82b6188f7f6ebe4e9cb68335c9093988826e69
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-12 13:14:58 +0530

    import java.util.*; class main {  public static void main(String args[])  {  Scanner sc=new Scanner(System.in);  float c;  float f;  f=sc.nextFloat();  c=(32+(9*f/5));  System.out.format("%.2f" ,c);  }  }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main
+{
+ public static void main(String args[])
+ {
+ Scanner sc=new Scanner(System.in);
+ float c;
+ float f;
+ f=sc.nextFloat();
+ c=(32+(9*f/5));
+ System.out.format("%.2f" ,c);
+ }
+ }
