61e300c7a06365a07052056eca5e9506cd8aacb6
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-17 15:08:53 +0530

    import java.util.Scanner; class Main {     public static boolean isprime(int n)     {         if(n<2)           return false;         int count=0;         for(int i=2;i<=(int)Math.sqrt(n);i++)           {               if(n%i==0)               count++;           }
            if(count==0)
               return true;
            else
               return false;
        }
        public static void main(String args[])
        {
            Scanner sc=new Scanner(System.in);
            int n,x[],i,k,count=0;
            n=sc.nextInt();
            x=new int[n];
            for(i=0;i<n;i++)
             x[i]=sc.nextInt();
            k=sc.nextInt();
            
            for(i=0;i<n;i++)
             {
                 if(isprime(x[i]))
                 {
                     if(x[i]>=k)
                    count++;
                 }
             }
            System.out.println(count);
        }
    }

---
+++ Primes_greater_than_k.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class Main
+{
+    public static boolean isprime(int n)
+    {
+        if(n<2)
+          return false;
+        int count=0;
+        for(int i=2;i<=(int)Math.sqrt(n);i++)
+          {
+              if(n%i==0)
+              count++;
+          }
+       
+        if(count==0)
+           return true;
+        else
+           return false;
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,k,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+         x[i]=sc.nextInt();
+        k=sc.nextInt();
+        
+        for(i=0;i<n;i++)
+         {
+             if(isprime(x[i]))
+             {
+                 if(x[i]>=k)
+                count++;
+             }
+         }
+        System.out.println(count);
+    }
+}
