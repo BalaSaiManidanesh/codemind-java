ae9fd3b61023cd395919cae9600dbebfd4a7c361
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-22 14:21:47 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,n,x[];         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=sc.nextInt();
            for(i=n-1;i>=0;i--)
              {
                  if(x[i]%2==0)
                  {
                      System.out.println(x[i]);
                      break;
                  }
              }
        }
    }

---
+++ Last_even_number_in_an_array.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,x[];
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=sc.nextInt();
+          
+        for(i=n-1;i>=0;i--)
+          {
+              if(x[i]%2==0)
+              {
+                  System.out.println(x[i]);
+                  break;
+              }
+          }
+    }
+}
