5be6a30ee58c6058ce4dade4dcf32cae6cc78598
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-23 14:44:26 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,n,x[],sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=sc.nextInt();
            for(i=0;i<n;i++)
            { 
                sum+=x[i];
            }
              System.out.println(sum); 
        }
    }

---
+++ Sum_of_Array.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,x[],sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=sc.nextInt();
+          
+        for(i=0;i<n;i++)
+        { 
+            sum+=x[i];
+        }
+          System.out.println(sum); 
+    }
+}
