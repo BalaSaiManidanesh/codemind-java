2b14c6377b92185999c4c7a7227caf98dd50cf0c
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-23 15:01:10 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],i,even=0,odd=0,absolute;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)         {             x[i]=sc.nextInt();         }         for(i=0;i<n;i++)         {             if(x[i]%2==0)             even=even+x[i];         }         for(i=0;i<n;i++)         {             if(x[i]%2==1)             odd=odd+x[i];         }         absolute=odd-even;         if(absolute<0)         {         absolute=Math.abs(absolute);         }         System.out.println(absolute);     } }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],i,even=0,odd=0,absolute;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+        {
+            x[i]=sc.nextInt();
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2==0)
+            even=even+x[i];
+        }
+        for(i=0;i<n;i++)
+        {
+            if(x[i]%2==1)
+            odd=odd+x[i];
+        }
+        absolute=odd-even;
+        if(absolute<0)
+        {
+        absolute=Math.abs(absolute);
+        }
+        System.out.println(absolute);
+    }
+}
