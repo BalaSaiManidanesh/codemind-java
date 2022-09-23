c6655f971a6235516e13972325c89f3b6d5ad9fb
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-09-23 14:49:15 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int i,n,x[],sum=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)           x[i]=sc.nextInt();
            for(i=0;i<n;i++)
            { 
                if(x[i]%2==1)
                sum+=x[i];
            }
              System.out.println(sum); 
        }
    }

---
+++ ODD_Sum.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int i,n,x[],sum=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+          x[i]=sc.nextInt();
+          
+        for(i=0;i<n;i++)
+        { 
+            if(x[i]%2==1)
+            sum+=x[i];
+        }
+          System.out.println(sum); 
+    }
+}
