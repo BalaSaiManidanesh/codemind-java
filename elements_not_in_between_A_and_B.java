423e6d0019e80d4743cf50be3eaef8808a3bfa99
Maddimsetty.Bala Sai Manidanesh <113014714+BalaSaiManidanesh@users.noreply.github.com>
2022-10-07 21:50:12 +0530

    import java.util.*; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,x[],a,b,i,count=0;         n=sc.nextInt();         x=new int[n];         for(i=0;i<n;i++)          x[i]=sc.nextInt();          a=sc.nextInt();          b=sc.nextInt();         for(i=0;i<n;i++)        {           if(x[i]<a || x[i]>b)           {            System.out.print(x[i]+" ");            count++;           }       }          if(count==0)            System.out.println("-1");     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,x[],a,b,i,count=0;
+        n=sc.nextInt();
+        x=new int[n];
+        for(i=0;i<n;i++)
+         x[i]=sc.nextInt();
+         a=sc.nextInt();
+         b=sc.nextInt();
+        for(i=0;i<n;i++)
+       {   
+          if(x[i]<a || x[i]>b)
+          {
+           System.out.print(x[i]+" ");
+           count++;
+          }
+      }
+         if(count==0)
+           System.out.println("-1");
+    }
+}
